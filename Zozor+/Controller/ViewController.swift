0bd415e66feeb12a5403886ee933a348bbf29d5a
janinjan <janin.culhaoglu@gmail.com>
2019-04-10 18:02:05 +0200

    Refactored into MVC pattern and added AlertDelegate protocol to Calculation class

---
+++ Zozor+/Controller/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  CountOnMe
+//
+//  Created by Ambroise COLLON on 30/08/2016.
+//  Copyright © 2016 Ambroise Collon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: - Properties
+    let calculation = Calculation()
+    
+    // MARK: - Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calculation.alertDelegate = self
+    }
+    
+    // MARK: - Outlets
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet var numberButtons: [UIButton]!
+    
+    // MARK: - Actions
+    @IBAction func tappedNumberButton(_ sender: UIButton) {
+        for (i, numberButton) in numberButtons.enumerated() {
+            if sender == numberButton {
+                textView.text = calculation.addNewNumber(i)
+            }
+        }
+    }
+    
+    @IBAction func plus() {
+        textView.text = calculation.calculate(with: "+")
+    }
+    
+    @IBAction func minus() {
+        textView.text = calculation.calculate(with: "-")
+    }
+    
+    @IBAction func equal() {
+        textView.text = textView.text + calculation.calculateTotal()
+    }
+}
+
+extension ViewController: AlertDelegate {
+    func presentAlert(title: String, message: String) {
+        let alertVC = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertVC.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+        self.present(alertVC, animated: true, completion: nil)
+    }
+}
---
+++ Zozor+/Model/Calculation.swift
@@ -0,0 +1,98 @@
+//
+//  Calculation.swift
+//  CountOnMe
+//
+//  Created by Janin Culhaoglu on 09/04/2019.
+//  Copyright © 2019 Ambroise Collon. All rights reserved.
+//
+
+import Foundation
+protocol AlertDelegate {
+    func presentAlert(title: String, message: String)
+}
+
+class Calculation {
+    // MARK: - Properties
+    var alertDelegate: AlertDelegate?
+    var stringNumbers: [String] = [String()]
+    var operators: [String] = ["+"]
+    var index = 0
+    var isExpressionCorrect: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                if stringNumbers.count == 1 {
+                    alertDelegate?.presentAlert(title: "Zéro!", message: "Démarrez un nouveau calcul !")
+                } else {
+                    alertDelegate?.presentAlert(title: "Zéro!", message: "Entrez une expression correcte !")
+                }
+                return false
+            }
+        }
+        return true
+    }
+    
+    var canAddOperator: Bool {
+        if let stringNumber = stringNumbers.last {
+            if stringNumber.isEmpty {
+                alertDelegate?.presentAlert(title: "Zéro!", message: "Expression incorrecte !")
+                return false
+            }
+        }
+        return true
+    }
+    
+    // MARK: - Methods
+    func addNewNumber(_ newNumber: Int) -> String {
+        if let stringNumber = stringNumbers.last {
+            var stringNumberMutable = stringNumber
+            stringNumberMutable += "\(newNumber)"
+            stringNumbers[stringNumbers.count-1] = stringNumberMutable
+        }
+        return updateDisplay()
+    }
+    
+    func calculate(with operatorSign: String) -> String {
+        if canAddOperator {
+            operators.append(operatorSign)
+            stringNumbers.append("")
+        }
+        return updateDisplay()
+    }
+    
+    func calculateTotal() -> String {
+        if !isExpressionCorrect {
+            return ""
+        }
+        var total = 0
+        for (i, stringNumber) in stringNumbers.enumerated() {
+            if let number = Int(stringNumber) {
+                if operators[i] == "+" {
+                    total += number
+                } else if operators[i] == "-" {
+                    total -= number
+                }
+            }
+        }
+        clear()
+        return "=\(total)"
+    }
+    
+    func updateDisplay() -> String {
+        var text = ""
+        for (i, stringNumber) in stringNumbers.enumerated() {
+            // Add operator
+            if i > 0 {
+                text += operators[i]
+            }
+            // Add number
+            text += stringNumber
+        }
+        return text
+    }
+    
+    func clear() {
+        stringNumbers = [String()]
+        operators = ["+"]
+        index = 0
+    }
+}
